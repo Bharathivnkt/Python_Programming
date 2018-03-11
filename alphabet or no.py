8479a573aff89e23d85dca2d4d1b712e4aa316c8
Bharathivnkt <36121630+Bharathivnkt@users.noreply.github.com>
2018-03-11 15:18:20 +0530

    Add files via upload

---
+++ alphabet or no.py
@@ -0,0 +1,5 @@
+inputs=input("enter the inputs")
+if inputs in('a','b','c','d','e','f','u','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' ):
+    print("alphabet")
+else:
+    print("no")
